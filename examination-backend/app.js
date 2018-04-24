3ce15a6d2ea034a23a028a4c9a8a09ca621aeffa
Heshan Padmasiri <hpheshan@gmail.com>
2018-04-24 06:53:47 +0530

    Added notifier when server is ready

--- examination-backend/app.js
+++ examination-backend/app.js
@@ -79,4 +79,5 @@ app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
+    console.log('Server ready on port:' + server.address().port);
 });
